5bdde7169e834a5902554e5b4a90f5b3606e23ce
mamalmaleki <mohammadalimalekie@gmail.com>
2023-04-23 15:59:11 +0330

    kafka & async messaging

---
+++ cmd/ratingingester/main.go
@@ -0,0 +1,68 @@
+package ratingingester
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/mamalmaleki/go_movie/rating/pkg/model"
+	"os"
+	"time"
+)
+
+func main() {
+	fmt.Println("Creating a kafka producer")
+
+	producer, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		panic(err)
+	}
+	defer producer.Close()
+
+	const fileName = "ratings_data.json"
+	fmt.Println("Reading rating events from file " + fileName)
+
+	ratingEvents, err := readRatingEvents(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	topic := "ratings"
+	if err := produceRatingEvents(topic, producer, ratingEvents); err != nil {
+		panic(err)
+	}
+
+	var timeout = 10 * time.Second
+	fmt.Println("Waiting " + timeout.String() + " until all events get produced")
+
+	producer.Flush(int(timeout.Milliseconds()))
+}
+
+func produceRatingEvents(topic string, producer *kafka.Producer, events []model.RatingEvent) error {
+	for _, event := range events {
+		encodedEvent, err := json.Marshal(event)
+		if err != nil {
+			return err
+		}
+
+		if err := producer.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          encodedEvent,
+		}, nil); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func readRatingEvents(fileName string) ([]model.RatingEvent, error) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	var ratings []model.RatingEvent
+	if err := json.NewDecoder(f).Decode(&ratings); err != nil {
+		return nil, err
+	}
+	return ratings, nil
+}
